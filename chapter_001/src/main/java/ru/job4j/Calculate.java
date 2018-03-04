c5ee10c85bf7aa80b18d8bf0dcb4f5e56f3ef7a5
AGlyzin <aglizin@yandex.ru>
2018-03-04 21:07:11 +0300

    1.3. JavDoc. Code convetion[#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,20 @@
 package ru.job4j;
 
+/**
+ * Class Calculate - Класс для каких-либо вычислений
+ * @author Artur Glyzin
+ * @since 04.03.2018
+ * @version 1
+*/
+
 public class Calculate {
+	
+    /**
+	 * Main Главный метод - с него всё начинается.
+     * Выводим в консоль - "Hello world"
+	 * @param args - args
+	*/
+	
 	public static void main(String[] args) {
 	System.out.println("Hello word!");
 	}
