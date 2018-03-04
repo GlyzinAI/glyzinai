c570bca817c2326b1367027615e882fcdb2e30f0
AGlyzin <aglizin@yandex.ru>
2018-03-04 23:16:39 +0300

    1.4. JUnit[#4122]

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,32 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test - тест для класса Calculate 
+ * @author Artur Glyzin
+ * @since 04.03.2018
+ * @version 1
+*/
+
+	public class CalculateTest {
+		
+		/**
+			Test echo
+		*/
+		@Test
+		public void whenTakeNameThenTreeEchoPlusName() {
+			String input = "Artur Glyzin";
+			String expect = "Echo, echo, echo : Artur Glyzin";
+			Calculate calc = new Calculate();
+			String result = calc.echo(input);
+			assertThat(result, is(expect));
+		}
+		
+	}
+	
+	
---
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculatetest task.
+*
+* @author Artur Glyzin (aglizin@yandex.ru)
+* @version 1
+* @since 04.03.2018
+*/
+package ru.job4j;
