29e0f155617cff25df7a673b0ffaa14f22686e68
AGlyzin <aglizin@yandex.ru>
2018-08-01 20:28:47 +0300

    Profession [#6837]

---
+++ chapter_002/src/main/java/ru/job4j/profession/Profession.java
@@ -0,0 +1,12 @@
+package ru.job4j.profession;
+
+public class Profession {
+    private String name;
+
+    private Profession profession;
+
+    public String getName(String name) {
+        return name;
+    }
+
+}
