ae52f6a8f118238b3f90bfb033cf7a244133a689
AGlyzin <aglizin@yandex.ru>
2018-09-24 23:30:57 +0300

    Refactor Chess [#792]

--- chapter_002/src/main/java/ru/job4j/chess/Pawn.java
+++ chapter_002/src/main/java/ru/job4j/chess/Pawn.java
@@ -26,10 +26,11 @@ public class Pawn extends Figure {
     @Override
     public Cell[] way(Cell source, Cell dest) throws ImposibleMoveException {
         Cell[] steps = new Cell[0];
+        if (behavior(source, dest)) {
+            throw new ImposibleMoveException("Imposible move!");
+        }
         if (dest.y == source.y + 1 && dest.x == source.x) {
             steps = new Cell[]{dest};
-        } else {
-            throw new ImposibleMoveException("Imposible move!");
         }
         return steps;
     }
@@ -44,5 +45,9 @@ public class Pawn extends Figure {
     @Override
     public Figure copy(Cell dest) {
         return new Pawn(dest);
+    }
+
+    public boolean behavior(Cell source, Cell dest) {
+        return !(dest.y == source.y + 1 && dest.x == source.x);
     }
 }
