ca8991b2f95f424c44611755f6c2be9d44315010
AGlyzin <aglizin@yandex.ru>
2018-10-15 00:30:32 +0300

    Конвертация листа массивов в один лист Integer [#10037]

--- chapter_003/src/main/java/ru/job4j/collections/list/ConvertList.java
+++ chapter_003/src/main/java/ru/job4j/collections/list/ConvertList.java
@@ -1,13 +1,14 @@
 package ru.job4j.collections.list;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * ConvertList - conversion ArrayList in array[][]
  *
  * @author Artur Glyzin
- * @version 2.0.
- * @since 13.10.2018
+ * @version 3.0.
+ * @since 14.10.2018
  */
 
 public class ConvertList {
@@ -21,23 +22,31 @@ public class ConvertList {
      */
 
     public int[][] toArray(List<Integer> list, int rows) {
-
         int cell = (int) (Math.ceil((double) list.size() / rows));
-
         int[][] array = new int[rows][cell];
-
         int count = 0;
-
         for (int i = 0; i < rows; i++) {
-
             for (int j = 0; j < cell; j++) {
-
                 if (count < list.size()) {
-
                     array[i][j] = list.get(count++);
                 }
             }
         }
         return array;
+    }
+
+    /**
+     * @param list - list int[]
+     * @return - list
+     */
+
+    public List<Integer> convert(List<int[]> list) {
+        List<Integer> arrList = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.get(i).length; j++) {
+                arrList.add(list.get(i)[j]);
+            }
+        }
+        return arrList;
     }
 }
--- chapter_003/src/test/java/ru/job4j/collections/list/ConvertListTest.java
+++ chapter_003/src/test/java/ru/job4j/collections/list/ConvertListTest.java
@@ -2,7 +2,9 @@ package ru.job4j.collections.list;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -11,8 +13,8 @@ import static org.hamcrest.core.Is.is;
  * CConvertListTest
  *
  * @author Artur Glyzin
- * @version 2.0.
- * @since 13.10.2018
+ * @version 3.0.
+ * @since 14.10.2018
  */
 
 public class ConvertListTest {
@@ -51,6 +53,17 @@ public class ConvertListTest {
                 {5, 6},
                 {7, 8}
         };
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenConvertListArray() {
+        ConvertList list = new ConvertList();
+        List<int[]> arrList = new ArrayList<>();
+        arrList.add(new int[]{1, 2, 3});
+        arrList.add(new int[]{5, 8, 13, 22});
+        List<Integer> result = list.convert(arrList);
+        List<Integer> expect = Arrays.asList(1, 2, 3, 5, 8, 13, 22);
         assertThat(result, is(expect));
     }
 }
