1408f7d82480e28ef9f1f7c5b654b487ee462a00
GlyzinAI <glizin.art@gmail.com>
2019-03-06 23:57:42 +0300

    Implementation Set + tests [#996]

--- chapter_005/src/test/java/ru/job4j/list/SimpleSetTest.java
+++ chapter_005/src/test/java/ru/job4j/list/SimpleSetTest.java
@@ -3,6 +3,7 @@ package ru.job4j.list;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 
 import static org.junit.Assert.*;
@@ -50,4 +51,24 @@ public class SimpleSetTest {
         assertThat(it.hasNext(), is(false));
     }
 
+    @Test
+    public void whenCheckSizeSet() {
+        set.add(11);
+        set.add(234);
+        set.add(123);
+        set.add(13);
+        set.add(16);
+        set.add(45);
+        assertThat(set.size(), is(10));
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenModificationSetThenThrowException() {
+        Iterator<Integer> it = set.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(5));
+        set.add(100);
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+    }
 }
