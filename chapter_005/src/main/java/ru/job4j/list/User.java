a5c951d7279c4c59eb4e81ee59cf96e45e05ac29
GlyzinAI <glizin.art@gmail.com>
2019-03-16 23:49:56 +0300

    model User for implementation HashMap

---
+++ chapter_005/src/main/java/ru/job4j/list/User.java
@@ -0,0 +1,63 @@
+package ru.job4j.list;
+
+import java.util.Calendar;
+
+/**
+ * Class User - model User.
+ *
+ * @author Artur Glyzin.
+ * @version 1.0.
+ * @since 16.03.2019.
+ */
+
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        User user = (User) o;
+
+        if (children != user.children) {
+            return false;
+        }
+        if (name != null ? !name.equals(user.name) : user.name != null) {
+            return false;
+        }
+        return birthday != null ? birthday.equals(user.birthday) : user.birthday == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + children;
+        result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
+        return result;
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/list/UserTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Class UserTest - test model User.
+ *
+ * @author Artur Glyzin.
+ * @version 1.0.
+ * @since 16.03.2019.
+ */
+
+public class UserTest {
+    @Test
+    public void outputMapWithoutOverrideMethods() {
+        User first = new User("Archi", 2, null);
+        User second = new User("Archi", 2, null);
+        Map<User, Object> map = new HashMap<>();
+        map.put(first, "first");
+        map.put(second, "second");
+        assertThat(first.equals(second), is(true));
+        System.out.println(map);
+        System.out.println("Hashcode user1 - " + first.hashCode());
+        System.out.println("Hashcode user2 - " + second.hashCode());
+    }
+}
