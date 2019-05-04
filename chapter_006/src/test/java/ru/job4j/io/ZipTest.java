3993dd183be49205d176f46926d0511792a0c17f
GlyzinAI <glizin.art@gmail.com>
2019-05-04 23:27:29 +0300

    ZipTest

---
+++ chapter_006/src/test/java/ru/job4j/io/ZipTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.io;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ZipTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void pack() {
+    }
+}
